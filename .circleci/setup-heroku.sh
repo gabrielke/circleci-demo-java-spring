ed70101376aae3d00bcba190115ce77e8ba67a77
Gabriel Akos <akos.gabriel@gmail.com>
2017-12-17 15:27:13 +0100

    command exploded

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -8,11 +8,11 @@
   machine api.heroku.com
     login $HEROKU_LOGIN
     password $HEROKU_API_KEY
-  EOF
+EOF
 
   cat >> ~/.ssh/config << EOF
   VerifyHostKeyDNS yes
   StrictHostKeyChecking no
-  EOF
+EOF
 
 
